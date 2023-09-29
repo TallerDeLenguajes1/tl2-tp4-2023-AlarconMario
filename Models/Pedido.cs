2063751d6cabfe3eea6f50b6308c6431c2b55007
Mario_Alarcon <48570998+Mario1294@users.noreply.github.com>
2023-09-29 00:07:51 -0300

    Creo cadete.json y pedido.json

--- Models/Pedido.cs
+++ Models/Pedido.cs
@@ -6,13 +6,13 @@ namespace spacePedido
     {
         private int nro;
         private string? obs;
-        //Cliente cliente;
+        Cliente cliente;
         private string? estado;
         int idCadete;
        
         public int Nro { get => nro; set => nro = value; }
         public string? Obs { get => obs; set => obs = value; }
-        //public Cliente Clientes { get => cliente; set => cliente = value; }
+        public Cliente Clientes { get => cliente; set => cliente = value; }
         public string? Estado { get => estado; set => estado = value; }
         public int IdCadete { get=> idCadete; set => idCadete = value; }
        
@@ -23,6 +23,7 @@ namespace spacePedido
             this.obs = _obs;
             this.estado = _estado;
             this.idCadete = _idCadete;
+            this.cliente = CrearClienteAleatorio();
             
         }
         public Pedido()
@@ -46,6 +47,29 @@ namespace spacePedido
             SinAsignar,
             EnCamino,
             Entregado
-        }  
+        } 
+
+        public Cliente CrearClienteAleatorio()
+        {
+            // Genera nombres aleatorios
+            string[] nombres = { "Juan", "María", "Carlos", "Ana", "Luis", "Laura", "Pedro", "Sofía" };
+            string nombreAleatorio = nombres[new Random().Next(nombres.Length)];
+
+            // Genera direcciones aleatorias
+            string[] direcciones = { "Calle A", "Avenida B", "Calle C", "Avenida D", "Calle E" };
+            string direccionAleatoria = direcciones[new Random().Next(direcciones.Length)];
+
+            // Genera números de teléfono aleatorios
+            string telefonoAleatorio = "381" + new Random().Next(1000, 9999);
+
+            // Genera datos de referencia aleatorios
+            string[] referencias = { "Referencia 1", "Referencia 2", "Referencia 3", "Referencia 4" };
+            string referenciaAleatoria = referencias[new Random().Next(referencias.Length)];
+
+            // Crea una instancia de Cliente con los datos aleatorios
+            Cliente clienteAleatorio = new Cliente(nombreAleatorio, direccionAleatoria, telefonoAleatorio, referenciaAleatoria);
+
+            return clienteAleatorio;
+        } 
     }    
 }
